4a2b8372d54cbe8a50221737862607c473066e87
nathanael18 <49261940+nathanael18@users.noreply.github.com>
2020-05-22 09:22:46 +0700

    Full Project

---
+++ EndlessRunnerYoutube/Assets/dont_destroy_audio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dont_destroy_audio : MonoBehaviour {
+
+    private static dont_destroy_audio instance = null;
+    public static dont_destroy_audio Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+}
